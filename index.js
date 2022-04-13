0b122d343509651578460b451033525f593a9357
Stefano Corra <stefano.corra@hotmail.com>
2022-04-12 21:26:06 -0400

    user commit

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
 
 dotenv.config()
+
+// app.get('/api/test', () => {
+//   console.log('test is successfull')
+// })
+
+app.use('/api/user', userRoute)
 
 mongoose
   .connect(process.env.MONGO_URL)
