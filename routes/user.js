0b337cf18eec7c9f94122e9ae795746a4bc2aac5
Stefano Corra <stefano.corra@hotmail.com>
2022-04-20 18:43:47 -0400

    jwt commit

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,19 @@
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+
 const router = require('express').Router()
 
 // router.get('/usertest', (req, res) => {
 //   res.send('user test is successfull')
 // })
+
+//UPDATE
+router.put('/:id', verifyTokenAndAuthorization, (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString()
+  }
+})
 
 module.exports = router
