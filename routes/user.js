b47020c771b0011b51738ade33e7e62600e72358
Stefano Corra <stefano.corra@hotmail.com>
2022-04-21 20:22:25 -0400

    update user commit

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const User = require('../models/User')
+const { verifyTokenAndAuthorization } = require('./verifyToken')
 
 const router = require('express').Router()
 
@@ -7,12 +8,23 @@ const router = require('express').Router()
 // })
 
 //UPDATE
-router.put('/:id', verifyTokenAndAuthorization, (req, res) => {
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
     ).toString()
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    )
+    res.status(200).json(updatedUser)
+  } catch (err) {
+    res.status(500).json(err)
   }
 })
 
